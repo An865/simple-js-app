856189c3199ad604cd9e47256dcbbbc334f44fc3
An865 <77985358+An865@users.noreply.github.com>
2021-02-15 16:59:40 -0700

    added pokemon array of objects

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,32 @@
-alert('Hello World');
-
-let favoriteFood = "mango";
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    type: ['electric']
+  },
+  {
+    name: 'Fearow',
+    height: 1.2,
+    type: ['flying','normal']
+  },
+  {
+    name: "Paras",
+    height: 0.3,
+    type: ['grass', 'bug']
+  },
+  {
+    name: "Poliwrath",
+    height: 1.3,
+    type: ['water', 'fighting']
+  },
+  {
+    name: "Graveler",
+    height: 1.0,
+    type: ['rock', 'ground']
+  }
+];
